890d0533f8fba4c6eeee716fe61c00a433f35cf9
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-08 16:21:59 +0200

    Initial commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,5 @@
+var Index = artifacts.require("./Index.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Index);
+};
---
+++ test/metacoin.js
@@ -0,0 +1,5 @@
+var Index = artifacts.require("./Index.sol");
+
+contract("Index", function(accounts) {
+  it("", function() {});
+});
