ccf05e6c267e0627b94e55d8365538c48bda0948
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-08 19:58:21 +0200

    Temp

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.24;
 
-contract Wallet {
+import "./Curve384.sol";
+
+contract Wallet is Curve384 {
     uint256 Pxlo;
     uint256 Pxhi;
     uint256 Pylo;
     uint256 Pyhi;
-    function Wallet(uint256 _Pxlo, uint256 _Pxhi, uint256 _Pylo, uint256 _Pyhi)
+    constructor(uint256 _Pxlo, uint256 _Pxhi, uint256 _Pylo, uint256 _Pyhi)
         public
     {
         Pxlo = _Pxlo;
@@ -13,14 +15,20 @@ contract Wallet {
         Pylo = _Pylo;
         Pyhi = _Pyhi;
     }
-    function sendETH(uint value, address receiver, uint256 xlo, uint256 xhi, uint256 ylo, uint256 yhi)
+    function sendETH(uint value, address receiver, uint256 rhi, uint256 rlo, uint256 shi, uint256 slo)
         public
     {
-        bytes32 hash = keccak256(value, receiver);
-        require(isValidEc384Signature(hash, xlo, xhi, ylo, yhi), "INVALID_SIGNATURE");
+        bytes32 hash = keccak256(abi.encodePacked(value, receiver));
+        require(verifySignature(hash, rhi, rlo, shi, slo), "INVALID_SIGNATURE");
         receiver.transfer(value);
     }
-    function isValidEc384Signature(bytes32 hash, uint256 xlo, uint256 xhi, uint256 ylo, uint256 yhi) public pure returns (bool) {
-        return true;
+    function verifySignature(bytes32 hash, uint256 rhi, uint256 rlo, uint256 shi, uint256 slo) public pure returns (bool) {
+        C384Elm memory pub = C384Elm({
+            xhi: Pxhi,
+            xlo: Pxlo,
+            yhi: Pyhi,
+            ylo: Pylo
+        });
+        return verify(pub, uint256(hash), rhi, rlo, shi, slo);
     }
 }
